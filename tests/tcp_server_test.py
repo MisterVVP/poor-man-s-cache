f40a059c66de279afd96962a471cd8d0f7791bbf
Vladimir Pavlov <mistervvp@outlook.com>
2025-02-13 06:04:16 +0200

    more CI-friendly and realistic load/functional testing

--- tests/tcp_server_test.py
+++ tests/tcp_server_test.py
@@ -9,9 +9,9 @@ from multiprocessing import Pool, cpu_count
 
 # configure logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
