f18d1c7a960fca32a5e5d907254e3bd574cb9fcb
Vladimir Pavlov <43521651+MisterVVP@users.noreply.github.com>
2025-04-21 21:56:43 +0300

    Remove busy looping in coroutine and fix readme (#2)
    * fix: remove busy looping
    
    * fix: small fixes
    
    ---------
    
    Co-authored-by: Vladimir Pavlov <mistervvp@outlook.com>

--- src/server/coroutines.cpp
+++ src/server/coroutines.cpp
@@ -2,15 +2,6 @@
 
 using namespace server;
 
-void ThreadSwitchAwaiter::await_suspend(std::coroutine_handle<> h)
-{
-    std::jthread& out = *p_out;
-    if (out.joinable()) {
-        throw std::runtime_error("ThreadSwitchAwaiter: output jthread parameter not empty");
-    }
-    out = std::jthread([h] { h.resume(); });
-}
-
 std::coroutine_handle<> server::HandleReqAwaiter::await_suspend(std::coroutine_handle<> h)
 {
     if (!h.done()) {
