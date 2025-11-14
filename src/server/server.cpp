f79700ff7ab0a014df7e1fb3d98aea3a4861baec
MisterVVP <mistervvp@live.ru>
2025-11-14 20:54:57 +0200

    teardown connection on RESP errors

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -366,6 +366,7 @@ HandleReqTask CacheServer::handleRequests()
 #endif
                 auto readResult = co_await readers[i];
                 if (readResult.operationResult == ReqReadOperationResult::Failure) {
+
                     //TODO: add special handling?
                     continue;
                 }
@@ -454,6 +455,7 @@ AsyncReadTask server::CacheServer::readRequestAsync(int client_fd)
                 connData.pendingRequests.clear();
                 connData.readBuffer.clear();
                 connData.bytesToErase = 0;
+                connManager->closeConnection(client_fd);
                 co_return ReadRequestResult{ ReqReadOperationResult::Failure };
             }
 
